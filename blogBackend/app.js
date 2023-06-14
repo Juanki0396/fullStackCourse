f1825871fe9e41ce6fb76039cfb41acaca01e3c0
Juan Carlos Aranda Huecas <juanki0396@gmail.com>
2023-06-14 22:34:26 +0200

    Added blogBackend from exercises 4.1 and 4.2

---
+++ blogBackend/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require("./utils/config")
+const blogRouter = require("./controllers/blogs")
+const logger = require("./utils/logger")
+const middlware = require("./utils/middleware")
+
+
+const app = express()
+
+logger.info("Connecting to MongoDB...")
+
+mongoose
+    .connect(config.MONGO_URI)
+    .then(() => {
+        logger.info("Connected to MongoDB")
+    })
+    .catch(() => {
+        logger.error("Failed to connect to MongoDB")
+    })
+
+
+app.use(cors())
+app.use(express.json())
+app.use(middlware.requestLogger)
+app.use("/api/blogs", blogRouter)
+app.use(middlware.unknownEndpoint)
+app.use(middlware.errorHandler)
+
+module.exports = app
